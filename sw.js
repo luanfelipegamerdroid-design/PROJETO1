a8acad431a1d332a518ff599a5cff7acdf186004
luanfelipegamerdroid-design <luanfelipegamerdroid@gmail.com>
2026-07-17 14:54:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'web-validade-v86'; // Atualizado para v86 para limpar o cache antigo e aplicar as correções
+const CACHE_NAME = 'web-validade-v87'; // Atualizado para v87 para limpar o cache antigo e aplicar as correções
 const urlsToCache = [
   './',
   './index.html',
